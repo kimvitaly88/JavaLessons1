300a80bb1f8319ac4b0e9c148bb756e728831af6
Vitaly Kim <vitalii_kim1@epam.com>
2021-06-10 17:25:45 +0300

    Add method

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,20 @@ public class Main {
     {
         return 0;
     }
+    public int sub(int a, int b)
+    {
+        return a+b;
+    }
+    public int div(int a, int b)
+    {
+        return a-b;
+    }
+    public int mult(int a, int b)
+    {
+        return a*b;
+    }
+    public int sqrt(int a, int b)
+    {
+        return a/b;
+    }
 }
