49123cb9d3798c99a083d1e3308278e8d10bb328
Vitaly Kim <vitalii_kim1@epam.com>
2021-06-10 18:25:50 +0300

    Add implementation for add operation with 3 parameters

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public int add(int a, int b)
     {
-        System.out.println("This is add method");
+        System.out.println("This is add method for 2 params");
         return 0;
     }
     public int sub(int a, int b)
@@ -19,5 +19,11 @@ public class Main {
     public int sqrt(int a, int b)
     {
         return a/b;
+    }
+
+    public int add(int a, int b, int c)
+    {
+        System.out.println("This is add method for 3 params");
+        return 0;
     }
 }
